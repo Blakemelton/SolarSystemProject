99df11b3a431344525875fcefe66aaf65202f738
blakemelton <blakemelton2591@gmail.com>
2021-11-16 14:48:59 -0500

    revised code

--- src/com/company/Planet89.java
+++ src/com/company/Planet89.java
@@ -3,6 +3,6 @@ package com.company;
 public class Planet89 {
 
     public static void main(String[] args) {
-        System.out.println("yo yo testing 123");
+        System.out.println("yo yo testing 82348234");
     }
 }
